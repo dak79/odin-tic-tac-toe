b4091d2f9ed984b4fb1af1aa344b5322826a0e93
Daniele Campari <danielecampari@gmail.com>
2022-09-13 14:52:26 +0200

    reset js logic

--- main.js
+++ main.js
@@ -1,52 +1,72 @@
-const player = function(name, symbol) {
-
+const player = (name, symbol, isPlaying) => {
     const getName = () => name;
     const getSymbol = () => symbol;
+    const getIsPlaying = () => isPlaying;
 
-    
-    // Get DOM elements
-    const spots = document.querySelectorAll('.spot');
-    
-    // Event Listeners
-    spots.forEach(spot => {
-        spot.addEventListener('click', play);
-    })
-    
-    // Choosen play
-    function play(event) {
-        if(!event.target.textContent) {
-            const index = event.target.dataset.index;
-            gameBoard.gameState(index, getSymbol());
-            gameBoard.render(spots);           
-        }
-    }
-            
     return {
         getName,
         getSymbol,
-        play
+        getIsPlaying
     }
-}
+    // Factory function for player: name, symbol, isPlaying, turn
 
-const gameBoard = (function () {
-    const board = [null, null, null, null, null, null, null, null, null];
+} 
+const player1 = player('jim', 'X', true);
+const player2 = player('josh', '0', false);
+
+
+
+const gameBoard = (() => {
+    const boardState = [
+        ["0", "1", "2"],
+        ["3", "4", "5"],
+        ["6", "7", "8"],
+    ];
+    console.log('B ' + player1.getName());
+    console.log('B ' + player1.getSymbol());
+    console.log('B ' + player1.getIsPlaying());
+    console.log('B ' + player2.getName());
+    console.log('B ' + player2.getSymbol());
+    console.log('B ' + player2.getIsPlaying());
+    const spots = document.querySelectorAll('.spot');
     
-        const gameState = (update, symbol) => {
-            board.splice(update, 1, symbol);
-        }
+    spots.forEach(spot => spot.addEventListener('click', play));
+    
+    function play(event) {
+        console.log(`click: Row ${event.target.dataset.row} - Column: ${event.target.dataset.column}`);
         
-        const render = (spots) => {
-            spots.forEach(spot => {
-                spot.textContent = board[spot.dataset.index];
-            })    
-        }
-        
-        return {
-            render,
-            gameState
-        }    
+    }
+    
+    const updateBoardState = () => {
+        // This function will update the array according to the move of player
+    }
+
+    const render = () => {
+        // This function will render the boardState
+    } 
+    
 })();
 
-const game = (function(){
-    const player1 = player('jim', 'X');     
+const game = (() => {
+    // This module manage start, turn, ending of game
+    console.log('G ' + player1.getName());
+    console.log('G ' + player1.getSymbol());
+    console.log('G ' + player1.getIsPlaying());
+    console.log('G ' + player2.getName());
+    console.log('G ' + player2.getSymbol());
+    console.log('G ' + player2.getIsPlaying());
 })();
+
+
+
+// Test
+console.log(player1.getName());
+console.log(player1.getSymbol());
+console.log(player1.getIsPlaying());
+console.log(player2.getName());
+console.log(player2.getSymbol());
+console.log(player2.getIsPlaying());
+
+
+
+
