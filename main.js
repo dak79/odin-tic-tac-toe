3d28a04d26200d336acdf077cd43a3c44880d684
Daniele Campari <danielecampari@gmail.com>
2022-09-15 21:47:24 +0200

    Add comments

--- main.js
+++ main.js
@@ -23,20 +23,25 @@ const player = (name, symbol, isPlaying) => {
 
 const setGame = (()=> {
 
+    // Create standard instances
     const playerOne = player('Player 1', 'X', true);
     const playerTwo = player('Player 2', '0', false);
 
+    // Get DOM Elements
     const main = document.querySelector('#display');
     const sectionGameBoard = document.querySelector('#gameBoard');
     const playFirst = document.querySelectorAll('input[name=radio-play-first]')
     const changeNameBtns = document.querySelectorAll('.btn-change');
     const btnStart = document.querySelector('#btn-start');
-
-   changeNameBtns.forEach(button => button.addEventListener('click', changePlayerName));
-
-   playFirst.forEach(button => button.addEventListener('change', setPlayFirst));
+    const playerName = document.querySelectorAll('.player-name-text');
+    
+    // Listeners
+    changeNameBtns.forEach(button => button.addEventListener('click', changePlayerName));
+    playFirst.forEach(button => button.addEventListener('change', setPlayFirst));
     
     function changePlayerName (event) {
+
+        // Create card for entering new name
         const background = document.createElement('div');
         const card = document.createElement('div');
         const newNameLabel = document.createElement('label');
@@ -60,17 +65,19 @@ const setGame = (()=> {
         btnSave.setAttribute('type', 'button');
         btnSave.textContent = 'Save';
         btnSave.classList.add('btn', 'btn-card');
-
         btnSave.addEventListener('click', setPlayerName);
 
         function setPlayerName () {
             const playerName = document.querySelector(`#${btnChange}`);
             playerName.textContent = newName.value;
             btnChange === 'player-one-name' ? playerOne.setName(newName.value) : playerTwo.setName(newName.value);
+            
+            // Remove card
             btnSave.removeEventListener('click', setPlayerName);
             background.remove();
         }
 
+        // Append card on DOM
         main.insertBefore(background, main.firstChild);
         background.appendChild(card);
         card.appendChild(newNameLabel);
@@ -109,6 +116,7 @@ const setGame = (()=> {
             
         }
 
+        // Reset winning display
         if (sectionGameBoard.className === 'invisible') {
             sectionGameBoard.classList.add('board');
             sectionGameBoard.classList.remove('invisible');
@@ -117,13 +125,14 @@ const setGame = (()=> {
 
     const endDisplay = (result) => {
 
-        sectionGameBoard.classList.add('invisible');
-        sectionGameBoard.classList.remove('board');
-
+        // Create result display
         const h1 = document.querySelector('h1')
         const wrapper = document.createElement('div');
         const winningText = document.createElement('p')
         const newGameInfo = document.createElement('p')
+        
+        sectionGameBoard.classList.add('invisible');
+        sectionGameBoard.classList.remove('board');
         
         wrapper.classList.add('winning-msg')
         
@@ -144,7 +153,8 @@ const setGame = (()=> {
             restartDisplay();
             wrapper.removeEventListener('click', restart);
         }
-        
+
+        // Append result display on DOM
         h1.after(wrapper);
         wrapper.appendChild(winningText);
         wrapper.appendChild(newGameInfo);
@@ -167,13 +177,13 @@ const gameBoard = (() => {
         null, null, null
     ];
 
+    // Get DOM elements
     const btnStart = document.querySelector('#btn-start');
     const spots = document.querySelectorAll('.spot');
     
+    // Listener
     btnStart.addEventListener('click', startGameState);
     
-    
-
     function startGameState() {
         setGame.restartDisplay();
         resetBoardState();
@@ -236,6 +246,9 @@ const gameBoard = (() => {
             setGame.playerOne.setIsPlaying(true);
             setGame.playerTwo.setIsPlaying(false);
         }
+
+        // Remove Listener
+        spots.forEach(spot => spot.removeEventListener('click', play));
     }
 
     // Manage final board
